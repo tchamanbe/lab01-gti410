bdf8f627d477cdef537e39a4d6ff606a96819582
parsenault23 <parsenault23@gmail.com>
2013-09-24 15:12:37 +0000

    documentation java

--- Lab1-Gti410/src/view/CMYKColorMediator.java
+++ Lab1-Gti410/src/view/CMYKColorMediator.java
@@ -22,13 +22,23 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	int imagesHeight;
 	ColorDialogResult result;
 	
+	/**
+	 * Constructeur
+	 * 
+	 * @param result(ColorDialogResult)
+	 * @param imagesWidth (int)
+	 * @param imagesHeight (int)
+	 */
 	CMYKColorMediator(ColorDialogResult result, int imagesWidth, int imagesHeight) {
 		this.imagesWidth = imagesWidth;
 		this.imagesHeight = imagesHeight;
 		
+		//Conversion du result (RGB) en valeur en pourcentage (de 0 a 1 au lieu de 0 a 255)
 		float redTemp = (float)result.getPixel().getRed()/255;
 		float greenTemp = (float)result.getPixel().getGreen()/255;
 		float blueTemp = (float)result.getPixel().getBlue()/255;
+		
+		//Algorithme de conversion de la couleur RGB (0 a 1) vers CMYK (0 a 1)
 		this.dark = 1-Math.max(redTemp, Math.max(greenTemp, blueTemp));
 		if(this.dark==1){
 			this.cyan=(1-redTemp-this.dark);
@@ -48,6 +58,8 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 		magentaImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
 		yellowImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
 		darkImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		//Appel des fonctions de traitement des images
 		computeCyanImage(cyan, magenta, yellow, dark);
 		computeMagentaImage(cyan, magenta, yellow, dark);
 		computeYellowImage(cyan, magenta, yellow, dark); 	
@@ -55,38 +67,55 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 	
 	
-	/*
+	/**
+	 * Methode qui update l'image si le slider est modifié
+	 * @params s(ColorSlider)
+	 * @params v(int)
 	 * @see View.SliderObserver#update(double)
-	 */
+	 **/
 	public void update(ColorSlider s, int v) {
 		boolean updateCyan = false;
 		boolean updateMagenta = false;
 		boolean updateYellow = false;
 		boolean updateDark = false;
+		
+		//Si le slider changé est le slider du cyan et que la valeur du cyan n'est pas la même que celle d'avant, reset le cyan 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)
 		if (s == cyanCS && (float)v/255 != cyan) {
 			cyan = (float)v/255;
 			updateMagenta = true;
 			updateYellow = true;
 			updateDark = true;
 		}
+		//Si le slider changé est le slider du magenta et que la valeur du magenta n'est pas la même que celle d'avant, reset le magenta 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)
 		if (s == magentaCS && (float)v/255 != magenta) {
 			magenta = (float)v/255;
 			updateCyan = true;
 			updateYellow = true;
 			updateDark = true;
 		}
+		//Si le slider changé est le slider du yellow et que la valeur du yellow n'est pas la même que celle d'avant, reset le yellow 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)
 		if (s == yellowCS && (float)v/255 != yellow) {
 			yellow = (float)v/255;
 			updateCyan = true;
 			updateMagenta = true;
 			updateDark = true;
 		}
+		//Si le slider changé est le slider du dark et que la valeur du dark n'est pas la même que celle d'avant, reset le dark 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)
 		if (s == darkCS && (float)v/255 != dark) {
 			dark = (float)v/255;
 			updateCyan = true;
 			updateMagenta = true;
 			updateYellow = true;
 		}
+		//Traitement des images selon les valeurs boolennes setter plus haut
 		if (updateCyan) {
 			computeCyanImage(cyan, magenta, yellow, dark);
 		}
@@ -100,6 +129,7 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 			computeDarkImage(cyan, magenta, yellow, dark);
 		}
 		
+		//Convertion de CMYK(pourcentage) a RBG(0 a 255) pour reset le pixel du result
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
@@ -107,116 +137,170 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 		result.setPixel(pixel);
 	}
 	
+	/**
+	 * Méthode qui traite l'image du cyan
+	 * @param cyan(float)
+	 * @param magenta(float)
+	 * @param yellow(float)
+	 * @param dark(float)
+	 */
 	public void computeCyanImage(float cyan, float magenta, float yellow, float dark) { 
+		//Convertion du CMYK (pourcentage) en RGB (0 a 255)
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
 		Pixel p = new Pixel(red, green, blue, 255); 
 		
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant le cyan)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le cyan selon l'etat de la boucle puis on convertit le cyan en rouge pour établir le pixel
 			cyan=((float)i/(float)imagesWidth);
 			p.setRed((int)(255*(1-cyan)*(1-dark))); 
 			int rgb = p.getARGB();
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				cyanImage.setRGB(i, j, rgb);
 			}
 		}
+		//Si le slider du cyan n'est pas null, update le slider
 		if (cyanCS != null) {
 			cyanCS.update(cyanImage);
 		}
 	}
 	
+	/**
+	 * Méthode qui traite l'image du magenta
+	 * @param cyan(float)
+	 * @param magenta(float)
+	 * @param yellow(float)
+	 * @param dark(float)
+	 */
 	public void computeMagentaImage(float cyan, float magenta, float yellow, float dark) {
+		//Convertion du CMYK (pourcentage) en RGB (0 a 255)
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
 		Pixel p = new Pixel(red, green, blue, 255); 
 		
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant le magenta)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le magenta selon l'etat de la boucle puis on convertit le magenta en vert pour établir le pixel
 			magenta=((float)i/(float)imagesWidth);
 			p.setGreen((int) (255*(1-magenta)*(1-dark))); 
 			int rgb = p.getARGB();
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				magentaImage.setRGB(i, j, rgb);
 			}
 		}
+		//Si le slider du magenta n'est pas null, update le slider
 		if (magentaCS != null) {
 			magentaCS.update(magentaImage);
 		}
 	}
 	
+	/**
+	 * Méthode qui traite l'image du yellow
+	 * @param cyan(float)
+	 * @param magenta(float)
+	 * @param yellow(float)
+	 * @param dark(float)
+	 */
 	public void computeYellowImage(float cyan, float magenta, float yellow, float dark) { 
+		//Convertion du CMYK (pourcentage) en RGB (0 a 255)
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
 		Pixel p = new Pixel(red, green, blue, 255); 
 		
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant le yellow)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le jaune selon l'etat de la boucle puis on convertit le jaune en bleu pour établir le pixel
 			yellow=((float)i/(float)imagesWidth);
 			p.setBlue((int) (255*(1-yellow)*(1-dark))); 
 			int rgb = p.getARGB();
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				yellowImage.setRGB(i, j, rgb);
 			}
 		}
+		//Si le slider du yellow n'est pas null, update le slider
 		if (yellowCS != null) {
 			yellowCS.update(yellowImage);
 		}
 	}
 	
+	/**
+	 * Méthode qui traite l'image du dark
+	 * @param cyan(float)
+	 * @param magenta(float)
+	 * @param yellow(float)
+	 * @param dark(float)
+	 */
 	public void computeDarkImage(float cyan, float magenta, float yellow, float dark) { 
+		//Convertion du CMYK (pourcentage) en RGB (0 a 255)
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
 		Pixel p = new Pixel(red, green, blue, 255); 
+		
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur(en variant le noir)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le noir selon l'etat de la boucle puis on convertit le CMYK en RGB pour établir le pixel(depend du noir)
 			dark=((float)i/(float)imagesWidth);
 			red=(int) (255*(1-cyan)*(1-dark));
 			green=(int) (255*(1-magenta)*(1-dark));
 			blue=(int) (255*(1-yellow)*(1-dark));
-			p.setRed((int)(255*(1-cyan)*(1-dark))); 
-			p.setGreen((int) (255*(1-magenta)*(1-dark))); 
-			p.setBlue((int) (255*(1-yellow)*(1-dark))); 
+			p.setRed(red); 
+			p.setGreen(green); 
+			p.setBlue(blue); 
 			int rgb = p.getARGB();
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				darkImage.setRGB(i, j, rgb);
 			}
 		}
+		//Si le slider du noir n'est pas null, update le slider
 		if (darkCS != null) {
 			darkCS.update(darkImage);
 		}
 	}
 	
 	/**
-	 * @return
+	 * Getter pour image du dark
+	 * @return darkImage (BufferedImage)
 	 */
 	public BufferedImage getDarkImage() {
 		return darkImage;
 	}
 	
 	/**
-	 * @return
+	 * Getter pour image du yellow
+	 * @return yellowImage (BufferedImage)
 	 */
 	public BufferedImage getYellowImage() {
 		return yellowImage;
 	}
 
 	/**
-	 * @return
+	 * Getter pour image du magenta
+	 * @return magentaImage (BufferedImage)
 	 */
 	public BufferedImage getMagentaImage() {
 		return magentaImage;
 	}
 
 	/**
-	 * @return
+	 * Getter pour image du cyan
+	 * @return cyanImage (BufferedImage)
 	 */
 	public BufferedImage getCyanImage() {
 		return cyanImage;
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider du cyan
+	 * @param slider(ColorSlider)
 	 */
 	public void setCyanCS(ColorSlider slider) {
 		cyanCS = slider;
@@ -224,7 +308,8 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider du magenta
+	 * @param slider(ColorSlider)
 	 */
 	public void setMagentaCS(ColorSlider slider) {
 		magentaCS = slider;
@@ -232,7 +317,8 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider du yellow
+	 * @param slider(ColorSlider)
 	 */
 	public void setYellowCS(ColorSlider slider) {
 		yellowCS = slider;
@@ -240,7 +326,8 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 		
 	/**
-	 * @param slider
+	 * Setter pour le slider du dark
+	 * @param slider(ColorSlider)
 	 */
 	public void setDarkCS(ColorSlider slider) {
 		darkCS = slider;
@@ -248,49 +335,58 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 	
 	/**
-	 * @return
+	 * Getter pour Cyan
+	 * @return cyan (float)
 	 */
 	public float getCyan() {
 		return cyan;
 	}
 
 	/**
-	 * @return
+	 * Getter pour Magenta
+	 * @return magenta (float)
 	 */
 	public float getMagenta() {
 		return magenta;
 	}
 
 	/**
-	 * @return
+	 * Getter pour yellow
+	 * @return yellow (float)
 	 */
 	public float getYellow() {
 		return yellow;
 	}
 	
 	/**
-	 * @return
+	 * Getter pour dark 
+	 * @return dark (float)
 	 */
 	public float getDark() {
 		return dark;
 	}
 
-
-	/* (non-Javadoc)
+	/**
+	 * Methode qui update l'image si le slider est modifié et qui traite les image si le changement est fait
+	 * sur un autre panel (RGB ou HSV)
 	 * @see model.ObserverIF#update()
-	 */
+	 **/
 	public void update() {
 		// When updated with the new "result" color, if the "currentColor"
-		// is aready properly set, there is no need to recompute the images.
+		// is already properly set, there is no need to recompute the images.
+		//Algorithme de conversion de CMYK vers RGB
 		int red=(int) (255*(1-cyan)*(1-dark));
 		int green=(int) (255*(1-magenta)*(1-dark));
 		int blue=(int) (255*(1-yellow)*(1-dark));
 		Pixel currentColor = new Pixel(red, green, blue, 255); 
 		if(currentColor.getARGB() == result.getPixel().getARGB()) return;
 		
+		//Conversion du result (RGB) en valeur en pourcentage (de 0 a 1 au lieu de 0 a 255)
 		float redTemp = (float)result.getPixel().getRed()/255;
 		float greenTemp = (float)result.getPixel().getGreen()/255;
 		float blueTemp = (float)result.getPixel().getBlue()/255;
+		
+		//Algorithme de conversion de la couleur RGB (0 a 1) vers CMYK (0 a 1)
 		this.dark = 1-Math.max(redTemp, Math.max(greenTemp, blueTemp));
 		if(this.dark==1){
 			this.cyan=0;
@@ -303,10 +399,13 @@ class CMYKColorMediator extends Object implements SliderObserver, ObserverIF {
 			this.yellow=(1-blueTemp-this.dark)/(1-this.dark);
 		}
 		
+		//Set la value des sliders selon la couleur(echelle de 255)
 		cyanCS.setValue((int)(cyan*255));
 		magentaCS.setValue((int)(magenta*255));
 		yellowCS.setValue((int)(yellow*255));
 		darkCS.setValue((int)(dark*255));
+		
+		//Applique le traitement sur les images
 		computeCyanImage(cyan, magenta, yellow, dark);
 		computeMagentaImage(cyan, magenta, yellow, dark);
 		computeYellowImage(cyan, magenta, yellow, dark);
--- Lab1-Gti410/src/view/HSVColorMediator.java
+++ Lab1-Gti410/src/view/HSVColorMediator.java
@@ -26,21 +26,35 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 	int imagesHeight;
 	ColorDialogResult result;
 	
+	/**
+	 * Constructeur
+	 * 
+	 * @param result (ColorDialogResult)
+	 * @param imagesWidth (int)
+	 * @param imagesHeight (int)
+	 */
 	HSVColorMediator(ColorDialogResult result, int imagesWidth, int imagesHeight) {
 		this.imagesWidth = imagesWidth;
 		this.imagesHeight = imagesHeight;
 		
+		//Conversion du result (RGB) en valeur en pourcentage (de 0 a 1 au lieu de 0 a 255)
 		redTemp = (float)result.getPixel().getRed()/255;
 		greenTemp = (float)result.getPixel().getGreen()/255;
 		blueTemp = (float)result.getPixel().getBlue()/255;
+		
+		//Determination de certaines valeurs requisent pour l'algorithme de convertion
 		float max = Math.max(redTemp,Math.max(greenTemp,blueTemp));
 		float min = Math.min(redTemp,Math.min(greenTemp,blueTemp));
 		float delta = max - min;
-		this.value = max;
 		
-		if(delta==0) this.saturation = 0;
-		else this.saturation = delta/max;
-
+		//Algorithme de conversion de la couleur RGB (pourcentage) vers HSV (0 a 360 pour H, pourcentage pour s et v)
+		this.value = max;
+		if(delta==0){
+			this.saturation = 0;
+		}
+		else{
+			this.saturation = delta/max;
+		}
 		if(redTemp==max) this.hue = 60 * (((greenTemp-blueTemp)/delta)%6);
 		else if(greenTemp==max) this.hue = 60 * (((blueTemp-redTemp)/delta)+2);
 		else this.hue = 60 * (((redTemp-greenTemp)/delta)+4);
@@ -51,34 +65,51 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 		hueImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
 		saturationImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
 		valueImage = new BufferedImage(imagesWidth, imagesHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		//Appel des fonctions de traitement des images
 		computeHueImage(hue, saturation, value);
 		computeSaturationImage(hue, saturation, value);
 		computeValueImage(hue, saturation, value); 	
 	}
 	
 	
-	/*
+	/**
+	 * Methode qui update l'image si le slider est modifié
+	 * @params s(ColorSlider)
+	 * @params v(int)
 	 * @see View.SliderObserver#update(double)
-	 */
+	 **/
 	public void update(ColorSlider s, int v) {
 		boolean updateHue = false;
 		boolean updateSaturation = false;
 		boolean updateValue = false;
+		
+		//Si le slider changé est le slider du hue et que la valeur du hue n'est pas la même que celle d'avant, reset le hue 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en angle de 0 a 360 (division par 255 suvit d'une multiplication par 360)
 		if (s == hueCS && (float)v*360/255 != hue) {
 			hue = (float)v*360/255;
 			updateSaturation = true;
 			updateValue = true;
 		}
+		//Si le slider changé est le slider du saturation et que la valeur du saturation n'est pas la même que celle d'avant, reset le saturation 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)
 		if (s == saturationCS && (float)v/255 != saturation) {
 			saturation = (float)v/255;
 			updateHue = true;
 			updateValue = true;
 		}
+		//Si le slider changé est le slider du value et que la valeur du value n'est pas la même que celle d'avant, reset le value 
+		//avec la nouvelle valeur et permet le traitement des autres couleurs
+		//NB:le v (value) est une valeur de 0 a 255 et doit être mis en pourcentage (division par 255)	
 		if (s == valueCS && (float)v/255 != value) {
 			value = (float)v/255;
 			updateHue = true;
 			updateSaturation = true;
 		}
+		
+		//Traitement des images selon les valeurs boolennes setter plus haut
 		if (updateHue) {
 			computeHueImage(hue, saturation, value);
 		}
@@ -89,6 +120,7 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 			computeValueImage(hue, saturation, value);
 		}
 		
+		//Convertion de hsv(0 a 360 et pourcentage) a RBG(0 a 255) pour reset le pixel du result
 		C = (value*saturation)*255;
 		X = ((value*saturation) * (1 - Math.abs(((hue / 60)%2) - 1)))*255;
 		M = (value - (value*saturation))*255;
@@ -102,8 +134,16 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 		result.setPixel(pixel);
 	}
 	
+	/**
+	 * Méthode qui traite l'image du hue
+	 * @param hue(float)
+	 * @param saturation(float)
+	 * @param value(float)
+	 */
 	public void computeHueImage(float hue, float saturation, float value) { 
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant le hue)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le hue selon l'etat de la boucle puis on convertit le HSV vers le RGB pour etablir le pixel
 			hue= (float)i*(360/(float)imagesWidth);
 			C = (value*saturation)*255;
 			X = ((value*saturation) * (1 - Math.abs(((hue / 60)%2) - 1)))*255;
@@ -115,17 +155,27 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 			else if(240<=hue && hue<300){redTemp=X;greenTemp=0;blueTemp=C;}
 			else{ redTemp=C;greenTemp=0;blueTemp=X;}
 			p=new Pixel((int)(redTemp+M),(int)(greenTemp+M),(int)(blueTemp+M));
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				hueImage.setRGB(i, j, p.getARGB());
 			}
 		}
+		//Si le slider du hue n'est pas null, update le slider
 		if (hueCS != null) {
 			hueCS.update(hueImage);
 		}
 	}
 	
+	/**
+	 * Méthode qui traite l'image de la saturation
+	 * @param hue(float)
+	 * @param saturation(float)
+	 * @param value(float)
+	 */
 	public void computeSaturationImage(float hue, float saturation, float value) {
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant la saturation)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine la saturation selon l'etat de la boucle puis on convertit le HSV vers le RGB pour etablir le pixel
 			saturation= ((float)i/(float)imagesWidth);
 			C = (value*saturation)*255;
 			X = ((value*saturation) * (1 - Math.abs(((hue / 60)%2) - 1)))*255;
@@ -137,17 +187,27 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 			else if(240<=hue && hue<300){redTemp=X;greenTemp=0;blueTemp=C;}
 			else{ redTemp=C;greenTemp=0;blueTemp=X;}
 			p=new Pixel((int)(redTemp+M),(int)(greenTemp+M),(int)(blueTemp+M));
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				saturationImage.setRGB(i, j, p.getARGB());
 			}
 		}
+		//Si le slider du saturation n'est pas null, update le slider
 		if (saturationCS != null) {
 			saturationCS.update(saturationImage);
 		}
 	}
 	
+	/**
+	 * Méthode qui traite l'image du value
+	 * @param hue(float)
+	 * @param saturation(float)
+	 * @param value(float)
+	 */
 	public void computeValueImage(float hue, float saturation, float value) { 
+		//Boucle qui set chaque pixel du Slider avec la bonne couleur (en variant le value)
 		for (int i = 0; i<imagesWidth; ++i) {
+			//Determine le value selon l'etat de la boucle puis on convertit le HSV vers le RGB pour etablir le pixel
 			value= ((float)i/(float)imagesWidth);
 			C = (value*saturation)*255;
 			X = ((value*saturation) * (1 - Math.abs(((hue / 60)%2) - 1)))*255;
@@ -159,38 +219,44 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 			else if(240<=hue && hue<300){redTemp=X;greenTemp=0;blueTemp=C;}
 			else{ redTemp=C;greenTemp=0;blueTemp=X;}
 			p=new Pixel((int)(redTemp+M),(int)(greenTemp+M),(int)(blueTemp+M));
+			//set le resultat de la couleur pour chaque pixel en hauteur(ligne verticale)
 			for (int j = 0; j<imagesHeight; ++j) {
 				valueImage.setRGB(i, j, p.getARGB());
 			}
 		}
+		//Si le slider du saturation n'est pas null, update le slider
 		if (valueCS != null) {
 			valueCS.update(valueImage);
 		}
 	}
 	
 	/**
-	 * @return
+	 * Getter pour image du value
+	 * @return valueImage (BufferedImage)
 	 */
 	public BufferedImage getValueImage() {
 		return valueImage;
 	}
 
 	/**
-	 * @return
+	 * Getter pour image de la saturation
+	 * @return saturationImage (BufferedImage)
 	 */
 	public BufferedImage getSaturationImage() {
 		return saturationImage;
 	}
 
 	/**
-	 * @return
+	 * Getter pour image du hue
+	 * @return hueImage (BufferedImage)
 	 */
 	public BufferedImage getHueImage() {
 		return hueImage;
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider du hue
+	 * @param slider(ColorSlider)
 	 */
 	public void setHueCS(ColorSlider slider) {
 		hueCS = slider;
@@ -198,7 +264,8 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider de la saturation
+	 * @param slider(ColorSlider)
 	 */
 	public void setSaturationCS(ColorSlider slider) {
 		saturationCS = slider;
@@ -206,7 +273,8 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 
 	/**
-	 * @param slider
+	 * Setter pour le slider de la value
+	 * @param slider(ColorSlider)
 	 */
 	public void setValueCS(ColorSlider slider) {
 		valueCS = slider;
@@ -214,33 +282,39 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 	}
 		
 	/**
-	 * @return
+	 * Getter pour hue
+	 * @return hue (float)
 	 */
 	public float getHue() {
 		return hue;
 	}
 
 	/**
-	 * @return
+	 * Getter pour saturation
+	 * @return satuation (float)
 	 */
 	public float getSaturation() {
 		return saturation;
 	}
 
 	/**
-	 * @return
+	 * Getter pour value
+	 * @return value (float)
 	 */
 	public float getValue() {
 		return value;
 	}
 
 
-	/* (non-Javadoc)
+	/**
+	 * Methode qui update l'image si le slider est modifié et qui traite les image si le changement est fait
+	 * sur un autre panel (RGB ou HSV)
 	 * @see model.ObserverIF#update()
-	 */
+	 **/
 	public void update() {
 		// When updated with the new "result" color, if the "currentColor"
-		// is aready properly set, there is no need to recompute the images.
+		// is already properly set, there is no need to recompute the images.
+		//Algorithme de conversion de HSV ver RGB
 		C = (value*saturation)*255;
 		X = ((value*saturation) * (1 - Math.abs(((hue / 60)%2) - 1)))*255;
 		M = (value - (value*saturation))*255;
@@ -253,17 +327,25 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 		Pixel currentColor = new Pixel((int)(redTemp+M),(int)(greenTemp+M),(int)(blueTemp+M));
 		if(currentColor.getARGB() == result.getPixel().getARGB()) return;
 		
+		//Conversion du result (RGB) en valeur en pourcentage (de 0 a 1 au lieu de 0 a 255)
 		redTemp = (float)result.getPixel().getRed()/255;
 		greenTemp = (float)result.getPixel().getGreen()/255;
 		blueTemp = (float)result.getPixel().getBlue()/255;
+		
+		//Determination de certaines valeurs requisent pour l'algorithme de convertion
 		float max = Math.max(redTemp,Math.max(greenTemp,blueTemp));
 		float min = Math.min(redTemp,Math.min(greenTemp,blueTemp));
 		float delta = max - min;
-		this.value = max;
 		
-		if(delta==0) this.saturation = 0;
-		else this.saturation = delta/max;
-
+		//Algorithme de conversion de la couleur RGB (pourcentage) vers HSV (0 a 360 pour H, pourcentage pour s et v)
+		this.value = max;
+		if(delta==0) 
+		{
+			this.saturation = 0;
+		}
+		else{
+			this.saturation = delta/max;
+		}
 		if(delta==0){
 			this.hue=0;
 		}
@@ -273,9 +355,12 @@ class HSVColorMediator extends Object implements SliderObserver, ObserverIF {
 			else this.hue = 60 * (((redTemp-greenTemp)/delta)+4);
 		}
 		
+		//Set la value des sliders selon la couleur(hue:valeur sur 360 et echelle de 255, saturation et value: echelle de 255)
 		hueCS.setValue((int)(this.hue/360*255));
 		saturationCS.setValue((int)(this.saturation*255));
 		valueCS.setValue((int)(this.value*255));
+		
+		//Applique le traitement sur les images
 		computeHueImage(hue, saturation, value);
 		computeSaturationImage(hue, saturation, value);
 		computeValueImage(hue, saturation, value);
